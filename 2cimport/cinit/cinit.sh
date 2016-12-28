7f6f42c8d62fb4ce5ea232f26f9d4a9d36e8e9bd
woosting <woosting@testert.lan>
2016-12-28 15:14:53 +0000

    getInput added and changed variable names.

--- 2cimport/cinit/cinit.sh
+++ 2cimport/cinit/cinit.sh
@@ -9,7 +9,37 @@
   RCOL='\033[0m'      #RESET color (default: terminal default)
 
 
-# EXECUTION
+# FUNCTION DEFINITION:
+
+  function printHelp () {
+    echo -e "USAGE: cinit [-u username] [-h]"
+    echo -e "         -u Specifies the target container's username (will be created)."
+    echo -e "         -h Prints this help."
+    echo -e "If no options are provided an interactive shell will commence."
+  }
+
+  function getInput () {
+    local OPTIND  u h option
+    while getopts u:h option
+    do
+      case "${option}"
+       in
+        u) TARGETUSERFLAG=(${OPTARG});;
+        h) printHelp
+           exit 0
+        ;;
+	\?) printHelp
+            exit 1
+        ;;
+      esac
+    done
+  }
+
+
+# LOGIC
+
+    getInput "$@"
+
     echo -e " "
     echo -e ">Installing wget:" && \
     echo -e " "
@@ -20,17 +50,20 @@
     echo -e " "
     wget -P /tmp/cinit/ https://raw.githubusercontent.com/woosting/baseInst/master/init.sh && \
     chmod 700 /tmp/cinit/init.sh && \
-
-    read -p "State the username: " TARGETUSERNAME
-    if [ -z "${TARGETUSERNAME}" ]; then
+    if [ -z "${TARGETUSERFLAG}" ]; then
+      read -p "State the username: " TARGETUSER
+    else
+      TARGETCONTAINER=${TARGETUSERFLAG}
+    fi
+    if [ -z "${TARGETUSER}" ]; then
       echo -e "${NOKCOL}No username received!${RCOL} Not executing genereal init script (you can still run it from within the container)!"
       exit 1
-    elif [ "${TARGETUSERNAME}" = "root" ]; then
-      echo -e "${NOKCOL}${TARGETUSERNAME} is not allowed!${RCOL} Not executing genereal init script (you can still run it from within the container)!"
+    elif [ "${TARGETUSER}" = "root" ]; then
+      echo -e "${NOKCOL}${TARGETUSER} is not allowed!${RCOL} Not executing genereal init script (you can still run it from within the container)!"
       exit 1
     else
       echo -e " "
-      echo -e ">Executing general init script creating ${TARGETUSERNAME} as a user:" && \
+      echo -e ">Executing general init script creating ${TARGETUSER} as a user:" && \
       echo -e " "
-      /tmp/cinit/init.sh -u ${TARGETUSERNAME}
+      /tmp/cinit/init.sh -u ${TARGETUSER}
     fi
