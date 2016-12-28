f5be54ba4be6fdebbe3782bb6333e52c9385827c
Willem Oosting <woosting@users.noreply.github.com>
2016-12-28 13:36:33 +0100

    Update cimport.sh

--- cimport.sh
+++ cimport.sh
@@ -23,21 +23,43 @@
   TARGETPATHPREFIX="/srv/lxc/"
   TARGETPATHPOSTFIX="/rootfs/tmp/"
 
-
+  function getInput () {
+    local OPTIND c u h option
+    while getopts c:u:h option
+    do
+      case "${option}"
+       in
+        c) TARGETCONTAINERFLAG=(${OPTARG});;
+        u) TARGETUSERFLAG=(${OPTARG});;
+        h) PRINTHELP=$((PRINTHELP+1));;
+        \?)
+          echo "Invalid option: -$OPTARG" >&2
+          ;;
+      esac
+      if [ ${PRINTHELP} -gt 0 ]; then
+        echo -e "USAGE: cimport [-c containername] [-u username] [-h]"
+        echo -e "         -c Specifies the target container's name (must exist)"
+        echo -e "         -u Specifies the target container's username (will be created)"
+        echo -e "         -h Prints this help"
+      fi
+    done
+          
+    
 # LOGIC
 
-# echo -e "State target container name:"
-# read CONTAINERNAME
-  read -p "State target container name: " CONTAINERNAME
-  
-  if [ -z "${CONTAINERNAME}" ]; then
+  if [ -z "${TARGETCONTAINERFLAG}" ]; then
+    read -p "State target container name: " TARGETCONTAINER
+  else
+    TARGETCONTAINER=${TARGETCONTAINERFLAG}
+  fi
+  if [ -z "${TARGETCONTAINER}" ]; then
     echo "Please provide a containername!"
     exit 1
-  elif [ ! -d "${TARGETPATHPREFIX}${CONTAINERNAME}" ]; then
-    echo "${CONTAINERNAME} is not a directory, does a container with that name exist?"
+  elif [ ! -d "${TARGETPATHPREFIX}${TARGETCONTAINER}" ]; then
+    echo "${TARGETCONTAINER} is not a directory, does a container with that name exist?"
     exit 1
   else
-    TARGETPATH="${TARGETPATHPREFIX}${CONTAINERNAME}${TARGETPATHPOSTFIX}"
+    TARGETPATH="${TARGETPATHPREFIX}${TARGETCONTAINER}${TARGETPATHPOSTFIX}"
     if [ ! -d "${TARGETPATH}" ]; then
       echo "${TARGETPATH} is not a (target) directory..."
       exit 1
@@ -45,11 +67,15 @@
       echo -e " "
       echo -e "Importing ${SOURCEPATH} to ${TARGETPATH}"
       cp -r ${SOURCEPATH} ${TARGETPATH}
-      echo -e "STARTING execution of scripts in ${CONTAINERNAME}:"
+      echo -e "STARTING execution of scripts in ${TARGETCONTAINER}:"
       echo -e " "
-      lxc-attach -n ${CONTAINERNAME} -- chmod 700 /tmp/cinit/cinit.sh && \
-      lxc-attach -n ${CONTAINERNAME} -- /tmp/cinit/cinit.sh && \
+      lxc-attach -n ${TARGETCONTAINER} -- chmod 700 /tmp/cinit/cinit.sh && \
+      if [ -z "${TARGETUSERFLAG}" ]; then
+        lxc-attach -n ${TARGETCONTAINER} -- /tmp/cinit/cinit.sh -u ${TARGETUSERFLAG} && \
+      else
+        lxc-attach -n ${TARGETCONTAINER} -- /tmp/cinit/cinit.sh && \
+      fi
       echo -e " " && \
-      echo -e "FINISHED executing of scripts in ${CONTAINERNAME}"
+      echo -e "FINISHED executing of scripts in ${TARGETCONTAINER}"
     fi
   fi
