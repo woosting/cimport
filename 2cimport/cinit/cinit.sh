f7a94f338878d3b3840d0c9c2d379eaf202e8cd2
Willem Oosting <woosting@users.noreply.github.com>
2016-12-28 16:44:34 +0100

    Update cinit.sh

--- 2cimport/cinit/cinit.sh
+++ 2cimport/cinit/cinit.sh
@@ -38,32 +38,32 @@
 
 # LOGIC
 
-    getInput "$@"
+  getInput "$@"
 
-    echo -e " "
-    echo -e ">Installing wget:" && \
-    echo -e " "
-    apt-get install -y wget && \
+  echo -e " "
+  echo -e ">Installing wget:" && \
+  echo -e " "
+  apt-get install -y wget && \
 
-    echo -e " "
-    echo -e ">Downloading general init script:" && \
-    echo -e " "
-    wget -P /tmp/cinit/ https://raw.githubusercontent.com/woosting/baseInst/master/init.sh && \
-    chmod 700 /tmp/cinit/init.sh && \
-    if [ -z "${TARGETUSERFLAG}" ]; then
-      read -p "State the username: " TARGETUSER
-    else
-      TARGETUSER=${TARGETUSERFLAG}
-    fi
-    if [ -z "${TARGETUSER}" ]; then
-      echo -e "${NOKCOL}No username received!${RCOL} Not executing genereal init script (you can still run it from within the container)!"
-      exit 1
-    elif [ "${TARGETUSER}" = "root" ]; then
-      echo -e "${NOKCOL}${TARGETUSER} is not allowed!${RCOL} Not executing genereal init script (you can still run it from within the container)!"
-      exit 1
-    else
-      echo -e " "
-      echo -e ">Executing general init script creating ${TARGETUSER} as a user:" && \
-      echo -e " "
-      /tmp/cinit/init.sh -u ${TARGETUSER}
-    fi
+  echo -e " "
+  echo -e ">Downloading general init script:" && \
+  echo -e " "
+  wget -P /tmp/cinit/ https://raw.githubusercontent.com/woosting/baseInst/master/init.sh && \
+  chmod 700 /tmp/cinit/init.sh && \
+  if [ -z "${TARGETUSERFLAG}" ]; then
+  read -p "State the username: " TARGETUSER
+  else
+  TARGETUSER=${TARGETUSERFLAG}
+  fi
+  if [ -z "${TARGETUSER}" ]; then
+  echo -e "${NOKCOL}No username received!${RCOL} Not executing genereal init script (you can still run it from within the container)!"
+  exit 1
+  elif [ "${TARGETUSER}" = "root" ]; then
+  echo -e "${NOKCOL}${TARGETUSER} is not allowed!${RCOL} Not executing genereal init script (you can still run it from within the container)!"
+  exit 1
+  else
+  echo -e " "
+  echo -e ">Executing general init script creating ${TARGETUSER} as a user:" && \
+  echo -e " "
+  /tmp/cinit/init.sh -u ${TARGETUSER}
+  fi
