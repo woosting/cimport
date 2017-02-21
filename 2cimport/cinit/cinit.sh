c5da148d6960065b20f4f57e8c088334edb67d00
Willem Oosting <woosting@users.noreply.github.com>
2017-02-21 22:43:01 +0100

    Update cinit.sh

--- 2cimport/cinit/cinit.sh
+++ 2cimport/cinit/cinit.sh
@@ -14,7 +14,6 @@
   function printHelp () {
      echo -e "USAGE: cinit [-u username] [-h]"
      echo -e "         -u Specifies the target container's username (will be created)."
-     echo -e "         -u Specifies the target container's username (will be created)."
      echo -e "         -h Prints this help."
      echo -e "If no options are provided an interactive shell will commence."
   }
