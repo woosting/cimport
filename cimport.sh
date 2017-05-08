2f8306f8009d6b4c5085cdde3569c287011e2ce0
Willem Oosting <woosting@users.noreply.github.com>
2017-05-08 21:49:47 +0200

    Config source path change

--- cimport.sh
+++ cimport.sh
@@ -19,7 +19,7 @@
 
 # CONFIGURATION
 
-  SOURCEPATH="/root/cimports/2cimport/*"
+  SOURCEPATH="/srv/scripts/cimports/2cimport/*"
   TARGETPATHPREFIX="/srv/lxc/"
   TARGETPATHPOSTFIX="/rootfs/tmp/"
 
